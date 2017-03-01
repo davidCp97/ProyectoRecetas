0e3406134ec8b45c8e216735fe3f809351d28014
SAngelCuadrosV <Sebas1208sk8@hotmail.com>
2017-03-01 12:08:52 -0500

    Se agregó funcionalidad a los botones e la pantalla de CrearReceta y BuscarNombreOTiempo, actualización de un archivo Leer con instrucciones para la Base de Datos

--- Proyecto/RecetasPro/src/Modelo/Conexion.java
+++ Proyecto/RecetasPro/src/Modelo/Conexion.java
@@ -9,8 +9,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
@@ -23,10 +26,7 @@ public class Conexion {
     static PreparedStatement pst;
     static ResultSet rs;
 
-    
-    
-    
-    
+    //Método base para realizar una consulta a la Base de Datos
     public void consultaABaseDeDatosBase(String consulta) {
         try {
             String dbURL = "jdbc:ucanaccess://C:\\Users\\Ingenieria\\Downloads\\Pre\\Nueva carpeta/Rest1.accdb"; //Dirección de donde está la base de datos
@@ -56,6 +56,7 @@ public class Conexion {
         }
     }
 
+    //Método base para realizar una inserción en la base de datos
     public void insertarABaseDeDatosBase(String insercion) {
         try {
             String dbURL = "jdbc:ucanaccess://C:\\Users\\Ingenieria\\Downloads\\Pre\\Nueva carpeta\\Rest1.accdb"; //Dirección de donde está la base de datos
@@ -73,6 +74,57 @@ public class Conexion {
             con.close();
         } catch (SQLException e) {
         }
+    }
+
+    //Método que regresa un entero, el cual es el código (Primary Key) de la última Receta
+    public int consultaUltimoID() {
+        int a = 0;
+        try {
+            String dbURL = "jdbc:ucanaccess://C:\\Users\\Ingenieria\\Downloads\\Pre\\Nueva carpeta/Rest1.accdb"; //Dirección de donde está la base de datos
+            String username = ""; //Usuario de la base de datos
+            String password = ""; //Password del usuario de la base de datos
+
+            con = DriverManager.getConnection(dbURL, username, password);
+            String query = "SELECT TOP 1 * FROM Receta ORDER BY codigo_receta DESC "; //query que solicita la última fila de datos
+            pst = con.prepareStatement(query);
+            rs = null;
+            try {
+                rs = pst.executeQuery();
+                while (rs.next()) {
+                    a = rs.getInt("codigo_receta"); //Se pide que solo regrese el último código de la receta.
+                }
+            } catch (SQLException sql) {
+            }
+            pst.close();
+            con.close();
+        } catch (SQLException e) {
+        }
+        return a;
+    }
+
+    //Método que devuelve en un arraylist la lista de los ingredientes totales.
+    public ArrayList consultaIngredientes() {
+        ArrayList result = new ArrayList<>();
+        try {
+            String dbURL = "jdbc:ucanaccess://C:\\Users\\Ingenieria\\Downloads\\Pre\\Nueva carpeta/Rest1.accdb"; //Dirección de donde está la base de datos
+            String username = ""; //Usuario de la base de datos
+            String password = ""; //Password del usuario de la base de datos
+            con = DriverManager.getConnection(dbURL, username, password);
+            String query = "Select * from Ingrediente"; //query que pide traer los datos de la tabla Ingrediente.
+            pst = con.prepareStatement(query);
+            rs = null;
+            try {
+                rs = pst.executeQuery();
+                while (rs.next()) {
+                    result.add(rs.getString("nombre_ingrediente"));//Se solicita únicamente la columna de Nombre_ingrediente
+                }
+            } catch (SQLException sql) {
+            }
+            pst.close();
+            con.close();
+        } catch (SQLException e) {
+        }
+        return result;
     }
 
 }
